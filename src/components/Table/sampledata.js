6a6d0eb12e8e63d607df1aa5463a046f88305efe
maiarakaki <67213065+maiarakaki@users.noreply.github.com>
2022-11-14 09:29:10 -0500

    created table component
    Table expects header and data props, separately.

---
+++ src/components/Table/sampledata.js
@@ -0,0 +1,18 @@
+module.exports=
+
+[{
+  "id": 1,
+  "first_name": "Ethelred",
+  "last_name": "Slowly",
+  "email": "eslowly0@google.es"
+},{
+  "id": 2,
+  "first_name": "Reta",
+  "last_name": "Woolmer",
+  "email": "rwoolmer1@miibeian.gov.cn"
+},{
+  "id": 3,
+  "first_name": "Arabel",
+  "last_name": "Pestor",
+  "email": "apestor2@bloglovin.com"
+}]
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Table from '../../components/Table';
+import catalogData from '../../components/Table/sampledata';
+
+const getHeadings = () => {
+    return Object.keys(catalogData[0]);
+}
 
 function View() {
     return (
         <>
             <Layout>
+                <Table theaders= {getHeadings()} tbody={catalogData}>
+
+                </Table>
             </Layout>            
         </>
     )
