122c54c1361cc70a007f0004b91f7e36f0ff5096
maiarakaki <67213065+maiarakaki@users.noreply.github.com>
2022-11-15 18:21:52 -0500

    tratando de sacar el JSON de la promise

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from '../../components/Layout';
 import Table from '../../components/Table';
 import catalogData from '../../components/Table/sampledata';
@@ -8,9 +8,27 @@ const getHeadings = () => {
     return Object.keys(catalogData[0]);
 }
 
+
 function View() {
- 
-    console.log(librosServices.datosHome());
+    const [mydata, setMydata] = useState();
+    function getData(){
+        librosServices.datosHome()
+            .then (response => {return response})
+            .catch (error => console.log(error));
+    }
+
+    setMydata(librosServices.datosHome().then ((response) => {return response} ));
+    console.log(mydata);
+//    console.log(librosServices.datosHome());
+
+    // const myData = librosServices.datosHome();
+    // console.log(myData.PromiseResult);
+    // console.log(librosServices.datosHome().then(json => console.log('rta', json)));
+   
+    
+    // setMydata(librosServices.datosHome());
+
+
     return (
         <>
             <Layout>
