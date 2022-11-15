a647986b60cb811b2e6643f08729146926394fef
Bautista Carvalho <bautidenueve@gmail.com>
2022-11-15 12:18:10 -0300

    Fix connections and warnings

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,38 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../../components/Layout';
 import Table from '../../components/Table';
-import catalogData from '../../components/Table/sampledata';
-import librosServices from '../../services/librosServices';
+import {getLibros} from '../../services/librosServices';
 
 const getHeadings = () => {
+    let catalogData;
     return Object.keys(catalogData[0]);
 }
 
 
 function View() {
     const [mydata, setMydata] = useState();
-    function getData(){
-        librosServices.datosHome()
-            .then (response => {return response})
-            .catch (error => console.log(error));
-    }
-
-    setMydata(librosServices.datosHome().then ((response) => {return response} ));
-    console.log(mydata);
-//    console.log(librosServices.datosHome());
-
-    // const myData = librosServices.datosHome();
-    // console.log(myData.PromiseResult);
-    // console.log(librosServices.datosHome().then(json => console.log('rta', json)));
-   
-    
-    // setMydata(librosServices.datosHome());
+    useEffect(() => {
+        const getData = async () => {
+            let response = await getLibros();
+            if(response.status) {
+                console.log('LLEGA', response);
+                setMydata(response.data);
+            }
+        }
+        getData();
+    }, []);
 
 
     return (
         <>
             <Layout>
-                <Table theaders= {getHeadings()} tbody={catalogData} />
+           
             </Layout>            
         </>
     )
