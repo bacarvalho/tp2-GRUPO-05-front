621bcf49fa51abab8a6b89bcdeea855717d6d870
maiarakaki <67213065+maiarakaki@users.noreply.github.com>
2022-11-15 19:49:32 -0500

    Update index.js

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -17,40 +17,43 @@ export const Table = () => {
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = tableInstance;
   
     return (
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                  ))
-                }
-              </tr>
-            ))
-          }
-  
-        </thead>
-  
-        <tbody {...getTableBodyProps()}>
-          {
-            rows.map(row => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {
-                    row.cells.map(cell=> {
-                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    })
-                  }
-  
-                </tr>
-              )
-            })
-          }
-  
-        </tbody>
-  
-      </table>
+        <div className='table-container'>
+            <table {...getTableProps()}>
+                    <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                            headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            ))
+                            }
+                        </tr>
+                        ))
+                    }
+            
+                    </thead>
+            
+                    <tbody {...getTableBodyProps()}>
+                    {
+                        rows.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                            {
+                                row.cells.map(cell=> {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })
+                            }
+            
+                            </tr>
+                        )
+                        })
+                    }
+            
+                    </tbody>
+            
+                </table>
+        </div>
+      
     )
   }
