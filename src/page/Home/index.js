e8b639fb108ecb564d3c743690c116ebfc9a0765
maiarakaki <67213065+maiarakaki@users.noreply.github.com>
2022-11-15 20:31:50 -0500

    removed unused sample ref from index

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-import catalogData from '../../components/Table/sampledata';
 import librosServices from '../../services/librosServices';
 import { Table } from '../../components/Table';
 
-const getHeadings = () => {
-    return Object.keys(catalogData[0]);
-}
 
 function View() {
  
