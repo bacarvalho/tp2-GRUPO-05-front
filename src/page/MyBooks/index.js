217b7195eb9f2a997d37df6623424f1a8c623b4c
maiarakaki <67213065+maiarakaki@users.noreply.github.com>
2022-11-21 12:02:38 -0500

    added isLogged condition for display

--- src/page/MyBooks/index.js
+++ src/page/MyBooks/index.js
@@ -6,10 +6,11 @@ import Table from '../../components/Table'
 import './styles.desktop.css'
 import { datosMisLibros } from '../../services/librosServices'
 import Cookies from "universal-cookie";
+import { isLoggedUser } from '../../services/OauthServices';
+
 
 function View() {
     const cookie = new Cookies();
-
     const [myData, setMydata] = useState([]);
     useEffect(() => {
         const getData = async () => {
@@ -21,18 +22,27 @@ function View() {
         getData();
     }, []);
     
-
     return(
         <>
             <Layout>
                 <h1>Mi Biblioteca</h1>
-                <Table data = {myData} columns = {COLUMNS}/>
-                <div className='buttons-container'>
-                    <button id='add-book'>Nuevo Libro</button>
+                <div className='my-books-container'>
+                    {isLoggedUser() ? (
+                    <span>
+                        <Table data = {myData} columns = {COLUMNS}/>
+                        <div className='buttons-container'>
+                        <button id='add-book'>Nuevo Libro</button>
+                        </div>
+                    </span>
+                    ) : 
+                    <span>Debes ingresar para acceder a este recurso</span>
+                    }
                 </div>
+                
             </Layout>
         </>
     )
+
 }
 
 export default View;
