416c9de31c1bdbb1ad9f94c73a4e60d6e31244d9
maiarakaki <67213065+maiarakaki@users.noreply.github.com>
2022-11-25 01:16:37 -0500

    fixed query syntax

--- src/components/Table/Filters.js
+++ src/components/Table/Filters.js
@@ -1,39 +1,58 @@
 import React, {useState} from 'react';
 import { searchService }  from '../../services/librosServices'
+import { useSearchParams } from 'react-router-dom';
 
 const mymap =new Map();
 
 async function handleSubmit(e, setMydata) {
-    let queryParams = '/?'
+    // e.preventDefault();
+    // let queryParams = '/?'
     
-    for (let [key, value] of  mymap.entries()) {
-        queryParams = queryParams + (key + "=" + value);
-    }
-    console.log('query',queryParams);
+    // for (let [key, value] of  mymap.entries()) {
+    //     queryParams = queryParams + (key + "=" + value +"&");
+    // }
+    // console.log('query',queryParams);
 
-    const response = await searchService(queryParams);
-	//const response = await LoginUser(username, password);
-	 if(response.status) {
-	// 	const cookie = new Cookies();
+    // const response = await searchService(queryParams);
+	// //const response = await LoginUser(username, password);
+	//  if(response.status) {
+	// // 	const cookie = new Cookies();
 	// 	cookie.set('login', response.data);
 	// 	navigate("/");
 	// } else {
 	// 	setIsDisabled(false);
 	// 	setErrorMessage(response.error.message);
-    setMydata(response.data);
-	 }
+    //     console.log("data",response.data);
+	//  }
 
+        e.preventDefault();
+    //const formData = new FormData(e.target);
+    const data = [...mymap.entries()];
+    const asString = '/?'+data
+        .map(x => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
+        .join('&');
+    console.log(asString);
+
+    const response = await searchService(asString.toLowerCase());
+    if (response.status) {
+        setMydata(response);
+        console.log(response);
+        //re-renderizar tabla con resultados
+        } else {
+        	//setIsDisabled(false);
+        	//setErrorMessage(response.error.message);
+            console.log("data",response.data);
+         }
 
   };
 
 export const Filters = ({columns, setMydata}) =>{
 
-    console.log(columns);
     return (
         <div className='filters-container'>
         <div className='filters-form'>
             Filtrar Resultados
-            <form id='filters-form' onSubmit= {handleSubmit(setMydata)}>
+            <form id='filters-form' onSubmit= {handleSubmit }>
                 {columns.map((column)=>{
                     if (! column.disableFilterBy) {
                         return ( 
