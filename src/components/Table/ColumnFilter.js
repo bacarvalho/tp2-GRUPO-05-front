edea12447f3a0e96567cda4ffc86170adcd736e2
Facofra <facundo_olleros@hotmail.com>
2022-11-25 21:43:51 -0300

    branch basura, no mergear ni a palos. es solo ejemplo de getGeneros

--- src/components/Table/ColumnFilter.js
+++ src/components/Table/ColumnFilter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InputField from './InputField.js'
 
 export const ColumnFilter = ({ column }) => {
   const { filterValue, setFilter } = column;
@@ -6,6 +7,7 @@ export const ColumnFilter = ({ column }) => {
     <span>
       <input value ={filterValue || ''} placeholder="filtrar por"
       onChange={e => setFilter(e.target.value)}/>
+      <InputField></InputField>
     </span>
   )
 }
